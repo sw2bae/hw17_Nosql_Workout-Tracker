e81bce982d0680bd4d56bc4bd8d43287ebb5f5db
Sang Bae <sw2.bae@gmail.com>
2020-07-06 23:41:23 -0500

    rev03_seedData init

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
