0658297a707270edcca9d0e19782f55158c0af18
Sang Bae <sw2.bae@gmail.com>
2020-07-06 22:47:26 -0500

    rev02_server.js Added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let mongoose = require("mongoose");
+let db = require("./ models");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
