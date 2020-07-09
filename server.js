ac8557d6c57dd62f265ae23bea03c6b78e6b6036
Sang Bae <sw2.bae@gmail.com>
2020-07-09 00:36:16 -0500

    rev04_path Added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 let mongoose = require("mongoose");
 let db = require("./ models");
+const path = require("path");
+
+
+
 
 const app = express();
 
@@ -11,13 +15,26 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 });
+
+
+
+
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
